35a68d26fb853ffa067eb4645df643cab63b29bd
Reza Makhdomi <remakhdomi@gmail.com>
2023-08-23 21:47:44 +0330

    print console

--- run.bat
+++ run.bat
@@ -1,10 +1,13 @@
 @echo off
 
+
 REM Pull from GitHub repository
 git pull
 
 REM Install Node.js app dependencies
+echo "installing dependencies ..."
 npm install
 
 REM Run the Node.js app
+echo "running project ..."
 npm start
